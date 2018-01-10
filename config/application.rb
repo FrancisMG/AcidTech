49971ebeb126b0309fe434e4077bd5264ef5515c
Francis Gonzales <francisgonzales.developer@gmail.com>
2018-01-10 18:29:45 +0800

    fixing error

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-config.assets.precompile += %w( ckeditor/* )
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
